18b2297c55f64042fe2029bad2a4ff7a8fd61a82
Thiago Felicio Alves <thiago.alves@phidelis.com.br>
2020-11-17 23:36:34 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/guardesuasfotosv2flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.guardesuasfotosv2flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sign_in.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+String name;
+String email;
+String imageUrl;
+
+
+Future<String> signInWithGoogle() async {
+  await Firebase.initializeApp();
+
+  final GoogleSignInAccount googleSignInAccount =  await new GoogleSignIn( scopes: [
+    'email', "https://www.googleapis.com/auth/photoslibrary.readonly"
+    ,
+  ]).signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final UserCredential authResult = await _auth.signInWithCredential(credential);
+  final User user = authResult.user;
+
+  if (user != null) {
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(user.photoURL != null);
+
+    name = user.displayName;
+    email = user.email;
+    imageUrl = user.photoURL;
+
+    if (name.contains(" ")) {
+      name = name.substring(0, name.indexOf(" "));
+    }
+
+
+    return '$user';
+  }
+
+  return null;
+}
+
+Future<void> signOutGoogle() async {
+  await googleSignIn.signOut();
+
+  print("User Signed Out");
+}
